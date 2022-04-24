6b5ee90f001c0e7f6d4b5942e716206a9865c21b
Salim Oussayfi <box@slimou.net>
2022-04-24 15:31:32 +0200

    first developments

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
@@ -3,14 +3,32 @@ package org.dieschnittstelle.mobile.android.skeleton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 import org.dieschnittstelle.mobile.android.skeleton.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewGroup viewRoot;
+    private TextView welcomeText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        viewRoot = findViewById(R.id.viewRoot);
+        welcomeText = findViewById(R.id.welcomeText);
+        welcomeText.setOnClickListener(view -> showMessage(getResources().getString(R.string.welcomeMessage2)));
+    }
+
+    private void showMessage(String msg) {
+//        Toast.makeText(this,msg, Toast.LENGTH_SHORT).show();
+        Snackbar.make(viewRoot, msg,Snackbar.LENGTH_SHORT).show();
     }
 }
