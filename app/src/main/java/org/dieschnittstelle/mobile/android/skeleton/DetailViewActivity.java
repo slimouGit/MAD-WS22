6d4d9ca43717d4b33308852b91f2f762718770cc
Salim Oussayfi <box@slimou.net>
2022-05-08 18:28:30 +0200

    27. April

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailViewActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/DetailViewActivity.java
@@ -10,6 +10,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import org.dieschnittstelle.mobile.android.skeleton.model.ToDo;
+
 public class DetailViewActivity extends AppCompatActivity {
     public static String ARG_ITEM = "item";
     private EditText itemNameText;
@@ -25,15 +27,20 @@ public class DetailViewActivity extends AppCompatActivity {
         saveNewItemButton = findViewById(R.id.fab_detail);
         saveNewItemButton.setOnClickListener(s -> onSaveItem());
 
-        String item = getIntent().getStringExtra(ARG_ITEM);
+        ToDo item = (ToDo) getIntent().getSerializableExtra(ARG_ITEM);
         if (item != null) {
-            itemNameText.setText(item);
+            itemNameText.setText(item.getName());
+            itemDescriptionText.setText(item.getDescription());
         }
     }
 
     private void onSaveItem() {
         Intent returnIntent = new Intent();
-        String item = itemNameText.getText().toString();
+        String name = itemNameText.getText().toString();
+        String description = itemDescriptionText.getText().toString();
+        ToDo item = new ToDo(name);
+        item.setDescription(description);
+
         returnIntent.putExtra(ARG_ITEM, item);
 
         setResult(Activity.RESULT_OK, returnIntent);
---
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/model/ToDo.java
@@ -0,0 +1,48 @@
+package org.dieschnittstelle.mobile.android.skeleton.model;
+
+import java.io.Serializable;
+
+public class ToDo implements Serializable {
+
+    private String name;
+    private String description;
+    private boolean checked;
+
+    public ToDo() {
+    }
+
+    public ToDo(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "DataItem{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+}
