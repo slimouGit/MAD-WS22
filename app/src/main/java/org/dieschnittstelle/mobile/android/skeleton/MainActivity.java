1e0ddb7ea6fcdc3f9b621bc92ce0eaec43d76286
Salim Oussayfi <box@slimou.net>
2022-05-08 18:28:53 +0200

    27. April

--- app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
+++ app/src/main/java/org/dieschnittstelle/mobile/android/skeleton/MainActivity.java
@@ -1,22 +1,43 @@
 package org.dieschnittstelle.mobile.android.skeleton;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import org.dieschnittstelle.mobile.android.skeleton.model.ToDo;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String LOGGER = "OverviewActivity";
     private ViewGroup viewRoot;
-    private ViewGroup listView;
     private FloatingActionButton addNewItemButton;
+
+    private ListView listView;
+    private ArrayAdapter<ToDo> listViewAdapter;
+    private List<ToDo> listViewItems = new ArrayList<>();
+
+
+    private ActivityResultLauncher<Intent> detailviewForNewActivityLauncher;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,20 +49,48 @@ public class MainActivity extends AppCompatActivity {
         addNewItemButton = findViewById(R.id.fab_add);
         addNewItemButton.setOnClickListener(v -> onAddNewItemButton());
 
-        Arrays.asList("Lorem", "Ipsum", "Olar", "Pipsum", "Enfis").forEach(
-                item -> {
-                    this.addListItemView(item);
-                });
+        listViewAdapter = new ArrayAdapter<>(this, R.layout.activity_main_listitem_view, listViewItems);
+        listView.setAdapter(listViewAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l) {
+                ToDo selectedItem = listViewAdapter.getItem(pos);
+                onListitemSelected(selectedItem);
+            }
+        });
+
+        InitialiseActivityResultLauncher();
+
+        Arrays.asList("Lorem", "Ipsum", "Olar", "Pipsum", "Enfis").
+                stream()
+    .map(name -> new ToDo(name))
+                .forEach( item -> this.addListItemView(item));
     }
 
-    private void addListItemView(String item) {
-        TextView listItemView = (TextView) getLayoutInflater().inflate(R.layout.activity_main_listitem_view, null);
-        listItemView.setText(item);
-        listView.addView(listItemView);
-        listItemView.setOnClickListener(v -> onListitemSelected(((TextView) v).getText().toString()));
+    private void InitialiseActivityResultLauncher() {
+        this.detailviewForNewActivityLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        if(result.getResultCode() == Activity.RESULT_OK){
+                            ToDo item = (ToDo) result.getData().getSerializableExtra(DetailViewActivity.ARG_ITEM);
+                            addListItemView(item);
+                        }
+                    }
+                }
+        );
     }
 
-    private void onListitemSelected(String item) {
+    private void addListItemView(ToDo item) {
+//        TextView listItemView = (TextView) getLayoutInflater().inflate(R.layout.activity_main_listitem_view, null);
+//        listItemView.setText(item);
+//        listView.addView(listItemView);
+//        listItemView.setOnClickListener(v -> onListitemSelected(((TextView) v).getText().toString()));
+        listViewAdapter.add(item);
+    }
+
+    private void onListitemSelected(ToDo item) {
         Intent detailviewIntent = new Intent(this, DetailViewActivity.class);
         detailviewIntent.putExtra(DetailViewActivity.ARG_ITEM, item);
         startActivity(detailviewIntent);
@@ -52,8 +101,27 @@ public class MainActivity extends AppCompatActivity {
         Snackbar.make(viewRoot, msg, Snackbar.LENGTH_SHORT).show();
     }
 
+    private static int CALL_DETAILVIEW_FOR_NEW_ITEM = 1;
+
     private void onAddNewItemButton() {
         Intent detailviewIntentForAddItem = new Intent(this, DetailViewActivity.class);
-        startActivity(detailviewIntentForAddItem);
+//        startActivityForResult(detailviewIntentForAddItem, CALL_DETAILVIEW_FOR_NEW_ITEM);
+        detailviewForNewActivityLauncher.launch(detailviewIntentForAddItem);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        Log.i(LOGGER, "requestCode: " + requestCode);
+        Log.i(LOGGER, "resultCode: " + resultCode);
+        Log.i(LOGGER, "data: " + data);
+        if (requestCode == CALL_DETAILVIEW_FOR_NEW_ITEM) {
+            if (resultCode == Activity.RESULT_OK) {
+                String name = data.getStringExtra(DetailViewActivity.ARG_ITEM);
+//                showMessage("revieved: " + name);
+//                addListItemView(name);
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 }
