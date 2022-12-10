33bc5885b80ffeaefe200f1da768bad6c8241dda
box@slimou.net <box@slimou.net>
2022-12-10 16:27:29 +0100

    validate email

--- app2/src/main/java/org/dieschnittstelle/mobile/android/skeleton/LoginActivity.java
+++ app2/src/main/java/org/dieschnittstelle/mobile/android/skeleton/LoginActivity.java
@@ -2,6 +2,9 @@ package org.dieschnittstelle.mobile.android.skeleton;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,26 +18,54 @@ public class LoginActivity extends AppCompatActivity {
     private EditText username;
     private EditText password;
     private Button login;
+//    String emailPattern = "[a-zA-Z0-9]+@[a-z]+\\.+[a-z]+";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        username=findViewById(R.id.username);
-        password=findViewById(R.id.password);
-        login=findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        login = findViewById(R.id.login);
+
+
+        username.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                if(Objects.equals(username.getText().toString(), "admin")&&Objects.equals(password.getText().toString(),"admin"))
-                {
-                    Toast.makeText(LoginActivity.this,"You have Authenticated Successfully",Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                    startActivity(intent);
-                }else
-                {
-                    Toast.makeText(LoginActivity.this,"Authentication Failed",Toast.LENGTH_LONG).show();
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                if (!username.toString().isEmpty() && username.getText().toString().trim().matches(emailPattern)) {
+                    Toast.makeText(LoginActivity.this, "Email Verified !", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Enter valid Email address !", Toast.LENGTH_SHORT).show();
                 }
             }
         });
+
+
+        login.setOnClickListener(new View.OnClickListener()
+
+    {
+        @Override
+        public void onClick (View view){
+        if (Objects.equals(username.getText().toString(), "admin") && Objects.equals(password.getText().toString(), "admin")) {
+            Toast.makeText(LoginActivity.this, "You have Authenticated Successfully", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_LONG).show();
+        }
     }
+    });
+}
 }
